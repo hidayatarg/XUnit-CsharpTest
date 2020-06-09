d6141a24e43c8bc70f0578f202ab7c1f9a72a8e7
Hidayet Argandabi <hidayet.argandabi@turk.net.tr>
2020-06-09 11:38:33 +0300

    String Tests

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -15,7 +15,78 @@ namespace GameEngine.Tests
 
             // Assert
             Assert.True(sut.IsNoob);
-
         }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            // Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+
+            // Act
+            sut.FirstName = "Hidayat";
+            sut.LastName = "Arghandabi";
+
+            // Assert
+            Assert.Equal("Hidayat Arghandabi", sut.FullName);
+        }
+
+        [Fact]
+        public void FullNameStartingWith()
+        {
+            // Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+
+            // Act
+            sut.FirstName = "Hidayat";
+            sut.LastName = "Arghandabi";
+
+            // Assert
+            Assert.StartsWith("Hidayat", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_Ignore_CaseAssert()
+        {
+            // Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+
+            // Act
+            sut.FirstName = "HIDAYAT";
+            sut.LastName = "ARGHANDABI";
+
+            // Assert
+            Assert.Equal("Hidayat Arghandabi", sut.FullName, ignoreCase: true);
+            // case ignore doesnot pay attention
+        }
+
+        [Fact]
+        public void CalculateFullName_SubStringContains()
+        {
+            // Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+
+            // Act
+            sut.FirstName = "Hidayat";
+            sut.LastName = "Arghandabi";
+
+            // Assert
+            Assert.Contains("at Ar", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullNameWithTitleCase()
+        {
+            // Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+
+            // Act
+            sut.FirstName = "Hidayat";
+            sut.LastName = "Arghandabi";
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
+        }
+
     }
 }
