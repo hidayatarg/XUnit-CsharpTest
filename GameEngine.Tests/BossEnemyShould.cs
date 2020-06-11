d41e0581fbad85571b8f515a5b71ef669cf423bc
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-11 09:43:25 +0300

    Writing Custom Output line using Writeline Console

--- GameEngine.Tests/BossEnemyShould.cs
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GameEngine.Tests
 {
     public class BossEnemyShould
     {
+
+        private readonly ITestOutputHelper _output;
+
+        public BossEnemyShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         [Trait("Category", "Boos")]
         public void HaveCorrectPower()
         {
+            _output.WriteLine("Creating Enemy");
             BossEnemy sut = new BossEnemy();
 
             Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
